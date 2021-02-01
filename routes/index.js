4f3de60d77aa802b74f4e192b990dc20bd0cb8f8
sarath <sarath@avantari.org>
2021-02-01 16:21:14 +0530

    code refractor

--- routes/index.js
+++ routes/index.js
@@ -3,17 +3,42 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) =>{
-  res.render('index', { title: 'Index', name:"nikhil" });
+  res.render('index', 
+      { 
+        title: 'Index', name:"nikhil", 
+        Github:"https://github.com/Nikhil3767", 
+        Linkedin:"https://in.linkedin.com/",
+        FaceBook: "https://www.facebook.com/"
+      }
+  );
 });
 
+/* GET About page. */
 router.get("/about", (req, res, next)=>{
-  res.render("about",  { title: "About" });
+  res.render("about",  
+    { 
+      title: "About" ,
+      Github:"https://github.com/Nikhil3767", 
+      Linkedin:"https://in.linkedin.com/",
+      FaceBook: "https://www.facebook.com/"
+    }
+  );
 });
 
+/* GET services page. */
 router.get("/services",(req, res, next)=>{
-  res.render("services", { title: "Services"})
+  res.render("services", 
+    { 
+      title: "Services",
+      Github:"https://github.com/Nikhil3767", 
+      Linkedin:"https://in.linkedin.com/",
+      FaceBook: "https://www.facebook.com/"
+    
+    }
+  )
 });
 
+/* GET contact page. */
 router.get("/contact",(req, res, next)=>{
   res.render("contact", 
   { 
@@ -23,13 +48,23 @@ router.get("/contact",(req, res, next)=>{
       {name:'name',type:'text',property:'required'},    //first field for the form
       {name:'email',type:'text',property:'required'},   //second field for the form
       {name:'mobile',type:'number',property:'required'}   //another field for the form
-      ]
+      ],
+      Github:"https://github.com/Nikhil3767", 
+      Linkedin:"https://in.linkedin.com/",
+      FaceBook: "https://www.facebook.com/"
   });
 });
 
-
+/* GET Projects page. */
 router.get("/projects",(req, res, next)=>{
-  res.render("projects", { title: "Project"})
+  res.render("projects", 
+    { 
+      title: "Project",
+      Github:"https://github.com/Nikhil3767", 
+      Linkedin:"https://in.linkedin.com/",
+      FaceBook: "https://www.facebook.com/"
+    }
+  );
 });
 
 module.exports = router;
